bcba6f97b70d8a18dd50b54171c7fa040145dba6
chunlea <ichunlea@me.com>
2019-02-01 11:51:34 +0800

    Create project with mix.new

---
+++ lib/permissions.ex
@@ -0,0 +1,18 @@
+defmodule Permissions do
+  @moduledoc """
+  Documentation for Permissions.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Permissions.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/permissions_test.exs
@@ -0,0 +1,8 @@
+defmodule PermissionsTest do
+  use ExUnit.Case
+  doctest Permissions
+
+  test "greets the world" do
+    assert Permissions.hello() == :world
+  end
+end
